f759d46bef14580889726c1427b90b54cf00a47f
simocar-bl <simone.carlassare@gmail.com>
2025-09-08 11:13:44 +0200

    script.js

--- script.js
+++ script.js
@@ -16,37 +16,3 @@ document.querySelectorAll(".slider").forEach(slider => {
     showSlide(index + 1);
   });
 });
-
-// Animazione cards quando entrano nello schermo
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("visible");
-    }
-  });
-}, { threshold: 0.1 });
-
-document.querySelectorAll(".card").forEach(card => {
-  observer.observe(card);
-});
-
-<script>
-  document.querySelectorAll('.slider').forEach(slider => {
-    const slides = slider.querySelector('.slides');
-    const slide = slider.querySelectorAll('.slide');
-    let index = 0;
-
-    const prev = slider.querySelector('.prev');
-    const next = slider.querySelector('.next');
-
-    function showSlide(i) {
-      if (i < 0) index = slide.length - 1;
-      else if (i >= slide.length) index = 0;
-      else index = i;
-      slides.style.transform = `translateX(${-index * 100}%)`;
-    }
-
-    prev.addEventListener('click', () => showSlide(index - 1));
-    next.addEventListener('click', () => showSlide(index + 1));
-  });
-</script>
